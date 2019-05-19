91c79aee828c806165a19f033a211efffe3017e8
krunal-lia <krunallia18@gmail.com>
2019-05-19 23:51:38 +0530

    Updates

---
+++ precache-manifest.23df62e2c0bf00792974d9a53b27af48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f48c05d80de2f10e8f56",
+    "url": "/color-preview/static/js/runtime~main.f48c05d8.js"
+  },
+  {
+    "revision": "4edda6acd86f2347f00a",
+    "url": "/color-preview/static/js/main.4edda6ac.chunk.js"
+  },
+  {
+    "revision": "6aabdae918c4d06099af",
+    "url": "/color-preview/static/js/1.6aabdae9.chunk.js"
+  },
+  {
+    "revision": "4edda6acd86f2347f00a",
+    "url": "/color-preview/static/css/main.f85bce9a.chunk.css"
+  },
+  {
+    "revision": "362a5bd17bc3a21c0fecaec363de8209",
+    "url": "/color-preview/index.html"
+  }
+];
